ee8674df5d60d8de1a0ae771ae8a3c3b6443eed0
Sanskriti <Sanskriti.24bcs10247@sst.scaler.com>
2025-10-03 11:31:38 +0530

    Importing the package @studio-freight/lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Lenis from "@studio-freight/lenis";
+
 import Home from "./pages/Home";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
-        {/* All sections (Hero, About, Services, Works, etc.) are inside Home Here we are just making it a route */}
+        {/* All sections (Hero, About, Services, Works, etc.) are inside Home.
+            Here we are just making it a route */}
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
